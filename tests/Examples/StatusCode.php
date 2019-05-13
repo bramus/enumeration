8c34bdee279d03206604e1f7f44890ade6ed4ac5
Bramus <bramus@bram.us>
2019-05-13 10:15:24 +0200

    ♻️ Correctly name Informational subset of status codes

--- tests/Examples/StatusCode.php
+++ tests/Examples/StatusCode.php
@@ -9,7 +9,7 @@ class StatusCode extends ComposedEnumeration
 	const __DEFAULT = 200;
 
 	public static $classes = [
-		'\Tests\Bramus\Enumeration\Examples\StatusCodes\Informal',
+		'\Tests\Bramus\Enumeration\Examples\StatusCodes\Informational',
 		'\Tests\Bramus\Enumeration\Examples\StatusCodes\Success',
 		'\Tests\Bramus\Enumeration\Examples\StatusCodes\Redirection',
 		'\Tests\Bramus\Enumeration\Examples\StatusCodes\ClientError',
--- tests/Examples/StatusCodes/Informational.php
+++ tests/Examples/StatusCodes/Informational.php
@@ -4,7 +4,7 @@ namespace Tests\Bramus\Enumeration\Examples\StatusCodes;
 
 use Bramus\Enumeration\Enumeration;
 
-abstract class Informal extends Enumeration
+abstract class Informational extends Enumeration
 {
 	const CONTINUE = 100;
 	const SWITCHING_PROTOCOLS = 101;
